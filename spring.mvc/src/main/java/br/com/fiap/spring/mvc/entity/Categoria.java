8f5be48979be469cf8444acd41714f2a34080029
gabrielmarcello <gabriel.marcellocf@gmail.com>
2025-08-08 22:31:27 -0300

    Creating book and repository

---
+++ spring.mvc/src/main/java/br/com/fiap/spring/mvc/entity/Categoria.java
@@ -0,0 +1,24 @@
+package br.com.fiap.spring.mvc.entity;
+
+
+public enum Categoria{
+    TERROR("Terror"),
+    ROMANCE("Romance"),
+    COMEDIA("Comédia"),
+    FICCAO("Ficção"),
+    SUSPENSE("Suspense"),
+    FANTASIA("Fantasia"),
+    BIOGRAFIA("Biografia"),
+    TECNOLOGIA("Tecnologia");
+
+    private String descricao;
+
+    Categoria(String descricao){
+        this.descricao = descricao;
+    }
+
+    public String getDescricao(){
+        return descricao;
+    }
+}
+
